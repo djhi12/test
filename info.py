d86d6e421883b6b895136e68a4d8083195e16150
djhi12 <ido20002@byui.edu>
2023-09-26 06:53:18 +0800

    Initial commit

---
+++ info.py
@@ -0,0 +1,21 @@
+print()
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Ask the user for their age
+age = input("How old are you? ")
+
+# Ask the user for their favorite color
+favorite_color = input("What is your favorite color? ")
+
+# Ask the user for their favorite food
+favorite_food = input("What is your favorite food? ")
+
+# Display the information provided by the user
+print("\nHere is the information you provided:")
+print("Name:", name)
+print("Age:", age)
+print("Favorite Color:", favorite_color)
+print("Favorite Food:", favorite_food)
+
+print()
